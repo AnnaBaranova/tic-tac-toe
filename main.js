d8f2a86e7d4d90deb14232f290442a176ad32791
Anna Baranova <asbaranova1@gmail.com>
2021-03-22 20:15:52 -0400

    finished game

--- main.js
+++ main.js
@@ -1,32 +1,135 @@
 // variables
-
-const colors = {
-    "0": "white",
-    "1": "red",
-    "-1": "blue"
+let game = {
+    player1: "Blue",
+    player2: "Red",
+    gameOver: false,
+    winner: "",
+    turn: -1,
+    board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
 }
 
-const board = ["0","0","0","0","0","0","0","0","0"];
 
-let turn = "1";
+const wins = [
+    [1, 1, 1,
+        0, 0, 0,
+        0, 0, 0],
+    [0, 0, 0,
+        1, 1, 1,
+        0, 0, 0],
+    [0, 0, 0,
+        0, 0, 0,
+        1, 1, 1],
+    [1, 0, 0,
+        1, 0, 0,
+        1, 0, 0],
+    [0, 1, 0,
+        0, 1, 0,
+        0, 1, 0],
+    [0, 0, 1,
+        0, 0, 1,
+        0, 0, 1],
+    [1, 0, 0,
+        0, 1, 0,
+        0, 0, 1],
+    [0, 0, 1,
+        0, 1, 0,
+        1, 0, 0],
 
-const table = document.querySelector("table");
-table.addEventListener("click", function(e){
+]
+
+
+$("#turn").html(`Your turn, ${game.player1}`)
+
+
+const $table = $("table");
+$table.on("click", function (e) {
     e.preventDefault();
-    let clickCell = e.target;
-    let index = clickCell.getAttribute("id");
-    console.log (e);
-    console.log(index);
-    board [index] = turn;
-    console.log (board);
-    if (turn === "1") {
-        turn = "-1";
-    } else {
-        turn = "1";
+    if (!game.gameOver) {
+        let clickCell = e.target;
+        let index = clickCell.getAttribute("id");
+
+        if (game.board[index] === 0) {
+            game.board[index] = game.turn;
+
+            checkWinner();
+            if (game.turn === -1) {
+                game.turn = 1;
+            } else {
+                game.turn = -1;
+            }
+        }
     }
+
+    print();
 })
 
+function print() {
+    for (i = 0; i < game.board.length; i++) {
+        if (game.board[i] === 1) {
+            $("#" + i).css({ "background-color": "red" });
+        } else if (game.board[i] === -1) {
+            $("#" + i).css({ "background-color": "blue" });
+        } else {
+            $("#" + i).css({ "background-color": "white" });
+        }
+    }
+    if (game.gameOver) {
+        $("#turn").html(game.winner)
+        return
+    }
+    if (game.turn === -1) {
+        $("#turn").html(`Your turn, ${game.player1.toUpperCase()}`)
+    } else {
+        $("#turn").html(`Your turn, ${game.player2.toUpperCase()}`)
+    }
+
+
+}
+
+function checkWinner() {
+
+    if (!game.gameOver) {
+        wins.forEach(function (win) {
+            let sum = 0;
+            win.forEach(function (el, idx) {
+                sum += (el * game.board[idx])
+            })
+            if (Math.abs(sum) === 3) {
+                console.log(sum)
+                game.gameOver = true;
+                if (game.turn === -1) {
+                    game.winner = `You win, ${game.player1.toUpperCase()}`;
+                } else {
+                    game.winner = `You win, ${game.player2.toUpperCase()}`;
+                }
+            }
+        })
+    }
+    if (!game.gameOver) {
+        let sum = 0;
+        game.board.forEach(function (el) {
+            sum += Math.abs(el);
+            if (sum === 9) {
+                console.log("tie");
+                game.gameOver = true;
+                game.winner = "Nobody won! TIE!";
+            }
+        })
+    }
+}
 
 
 
+
+$(".btn").on("click", function (e) {
+    // console.log("btn");
+    e.preventDefault();
+    game.gameOver = false;
+    game.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    game.winner = "";
+    game.turn = -1;
+    print();
+})
+
+print();
 
